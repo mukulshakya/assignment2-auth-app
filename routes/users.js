285ceba40d8e29463b0e3a27be442cf49869d03e
Mukul Shakya <muklshakya123@gmail.com>
2019-07-13 17:13:47 +0530

    added forgotpassword route

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,7 @@ const passport = require('passport');
 const User = require('../models/user');
 const {auth} = require('../config/auth');
 
-
-/* GET users listing. */
-router.get('/', auth(), (req, res, next) => {
+router.get('/', auth(), (req, res) => {
   res.render('welcome', {username: req.session.passport.user.username});
 });
 
@@ -21,11 +19,6 @@ router.post('/register',async (req, res) => {
 
   let duplicateUser = await User.findOne({username: req.body.username});
   
-  // User.findOne({username: req.body.username}).then((user) => {
-  //   if(user) return res.send({msg:"danger",msg1:"username not available"});
-  // }).catch(() => {
-  //   return res.send({msg:"danger",msg1:"Unexpected error"});
-  // });
   if(duplicateUser) {
     res.send({msg:"danger",msg1:"username not available"});
   }
@@ -54,17 +47,63 @@ router.post('/register',async (req, res) => {
 
 
 // Login Form
-router.get('/login', function(req, res){
+router.get('/login', (req, res) => {
   res.render('login', {title: 'Login'});
 });
 
 // Login Process
-router.post('/login',  passport.authenticate('local', { failureRedirect: '/login' }), function(req, res) {
-  res.redirect('/');
+router.post('/login',  passport.authenticate('local', { failureRedirect: '/login' }), (req, res) => {
+  res.send({login: 'success'});
+});
+
+// Forgot password Form
+router.get('/forgotpassword', (req, res) => {
+  res.render('forgotpassword', {title: 'Forgot password'});
+});
+
+// Forgot password process
+router.post('/forgotpassword', async (req, res) => {
+  let user = await User.findOne({username: req.body.username});
+
+  if(!user) {
+    res.send({msg:"danger",msg1:"username not valid"})
+  }
+  else {
+    bcrypt.compare(req.body.oldPassword, user.password, function(err, isMatch) {
+      if(err) throw err;
+      if(isMatch){
+        console.log('old password matched');
+        bcrypt.genSalt(10, function(err, salt){
+          bcrypt.hash(req.body.newPassword, salt, function(err, hash){
+            if(err){
+              return res.send({msg:"danger",msg1:"some error occurred"});
+            }
+    
+            // let newUser = new User({username: req.body.username, password: hash});
+            user.password = hash;
+    
+            user.save()
+              .then((user) => {
+                console.log(user);
+                res.send({msg:"success",msg1:"Password updated"});
+              })
+              .catch((err) => {
+                console.log(err);
+                res.send({msg:"danger",msg1:"please try again"});
+              });
+          });
+        }); 
+      } 
+      else {
+        console.log('old wrong password');
+        res.send({msg:"danger",msg1:"old password incorrect"});
+      }
+    });
+  }
 });
 
 // logout
-router.get('/logout', function(req, res){
+router.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/login');
 });
