df1fd07b8a598c082d4f534103fcd9e47d246a86
Mukul Shakya <muklshakya123@gmail.com>
2019-07-13 18:03:53 +0530

    set NODE_ENV for production

--- config/db.js
+++ config/db.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose')
 
-let conUrl = 'mongodb://127.0.0.1:27017/assign2-auth' || 'mongodb://mukul:ms1234@ds129762.mlab.com:29762/heroku_vkb4dhv1';
+let conUrl = "";
+
+if(process.env.NODE_ENV == "production"){
+    conUrl = 'mongodb://mukul:ms1234@ds129762.mlab.com:29762/heroku_vkb4dhv1';
+} 
+else if (process.env.NODE_ENV == "development"){
+    conUrl = 'mongodb://127.0.0.1:27017/assign2-auth';
+} 
+
+console.log(conUrl)
 
 mongoose.connect(conUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
-})
+});
